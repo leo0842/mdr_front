cb779025ab485d821892ca20cb90d9b3b83112d3
Yu Taek <lcpnine@gmail.com>
2020-09-22 22:49:52 +0900

    signup 회원가입 기능 구현

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -3,36 +3,46 @@
     <h1>회원가입</h1>
     <div class='form-login'>
       <label for="email">이름 / 유저명</label>
-      <input type="text" id="name" class="form-control" v-model="user.name"><br>
+      <input type="text" id="nickname" class="form-control" v-model="user.nickname"><br>
       <label for="email">이메일</label>
       <input type="text" id="email" class="form-control" v-model="user.email"><br>
       <label for="password">비밀번호</label>
       <input type="password" id="password" class="form-control" v-model.lazy="user.password"><br>
       <label for="password-check">비밀번호 확인</label>
       <input type="password" id="password-check" class="form-control" v-model.lazy="user.passwordCheck"><br>
-      <button class="btn btn-primary" @click.prevent="submitted">회원가입</button>
+      <button class="btn btn-primary" v-on:click="submitted">회원가입</button>
     </div>
   </div>
 </template>
-
+  
 <script>
 export default {
   name: 'SignUp',
   data(){
     return {
       user: {
-        name:"",
+        nickname:"",
         email:"",
-        password:"",
-        passwordCheck:""
-      }
+        password:""
+      },
+      data: ''
     }
   },
   methods: {
-    submitted(){
-      this.isSubmitted = true;
-      this.$router.push({name: 'Home'})
-    }
+    submitted() {
+  // POST request using fetch with set headers
+  const requestOptions = {
+    method: 'POST',
+    headers: { 
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(this.user)
+  };
+  console.log(this.user);
+  fetch('http://localhost:8000/user/signup/', requestOptions)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
   }
 }
 </script>
